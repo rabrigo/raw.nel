602ac2aadd47d5224e8076b1e11ba8e015827be0
the rawness <ronnelabrigo@gmail.com>
2023-10-24 23:56:18 -0700

    createserver and express

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
