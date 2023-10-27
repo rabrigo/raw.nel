2eb798821ad6496dd77116aa1925bb79412053d7
the rawness <ronnelabrigo@gmail.com>
2023-10-27 13:34:46 -0700

    back to foundation of app

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
-import path from 'path';
+// import path from 'path';
 import express from 'express';
 // import * as ejs from 'ejs';
 import http from 'http';
 import bodyParser from 'body-parser';
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3000;
 
 // get root directory for paths
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// beginning here is from Microsoft example
+// const on404Error = (req, res, next) => {
+//   next(createError(404));
+// };
+
+// const onRouteErrors = (err, req, res, next) => {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error', err);
+// };
+
+// const checkTrailingSlash = (req, res, next) => {
+//   const trailingSlashUrl = req.baseUrl + req.url;
+//   if (req.originalUrl !== trailingSlashUrl) {
+//     res.redirect(301, trailingSlashUrl);
+//   } else {
+//     next();
+//   }
+// };
+
+// app.use(checkTrailingSlash);
+
+// ends here
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 // Configure view engine to return HTML
-app.set('public', path.join(__dirname, './public'));
-// app.set('public engine', 'html');
-// app.engine('.html', ejs.__express);
-app.use(express.static(path.join(__dirname, 'public')));
+// app.set('public', path.join(__dirname, './public'));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/', (req, res) => {
-//   res.send('It works!!');
-// });
+app.get('/', (req, res) => {
+  res.send('It works!!');
+});
 
 const server = http.createServer(app);
 
